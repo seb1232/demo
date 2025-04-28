94389169f79f4ec78d2ac9601908f85168608da8
Sebin Anto Padinjarekara <sebin160@gmail.com>
2025-04-28 10:08:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit as st
 pg=st.navigation([st.Page("SprintTaskPlanner.py"),st.Page("RetrospectiveAnalysisTool.py")])
+st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
 pg.run()
